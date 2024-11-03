b0abd5fb1891b1b82e6ed7f74d1ef90906e15ade
Muhammed Shan <95335433+ShanSuharban@users.noreply.github.com>
2024-11-03 18:06:29 +0530

    Delete java/access_modifiers/Accesss.java

--- java/access_modifiers/Accesss.java
+++
@@ -1,9 +0,0 @@
-import Packag.Accessprivate;
-public class Accesss extends Accessprivate
-{
-        public static void main(String[] args)
-        {
-                Accesss a = new Accesss();
-                a.display();
-        }
-}
