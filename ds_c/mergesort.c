b5f4026169c3b4dd7b277e79cf2ece0ed852ad6a
Muhammed Shan <95335433+ShanSuharban@users.noreply.github.com>
2024-11-03 20:17:09 +0530

    added mergesort
    Added MergeSort

---
+++ ds_c/mergesort.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+
+void Merge (int a[] , int low, int mid , int high ) 
+{
+	int i=low, j=mid+1, k=low;
+	int b[100];
+	while (i<=mid && j<=high )
+	{
+		if (a[i]<a[j])
+		{
+			b[k]=a[i];
+			i++;k++;
+		}
+
+		else 
+		{
+			b[k]=a[j];
+			j++;k++;
+		}
+	}
+
+	while (i<=mid)
+	{
+		b[k]=a[i];
+		i++;k++;
+	}
+	while (j<=high)
+	{
+		b[k]=a[j];
+		j++;k++;
+	}
+
+	for (i=low;i<=high;i++)
+		a[i]=b[i];
+
+}
+
+void MergeSort (int a[], int low, int high)
+{
+	if (high>low)
+	{
+		int mid=(low+high)/2;
+		MergeSort (a,low,mid);
+		MergeSort (a,mid+1,high);
+		Merge(a,low,mid,high);
+	}
+}
+
+void main () 
+{
+	int n,i;
+	printf ("\nEnter the size of array:  ");
+	scanf ("%d",&n);
+	int a[n];
+	printf ("\nEnter the array elements: ");
+	for (i=0;i<n;i++)
+		scanf ("%d",&a[i]);
+
+	MergeSort (a,0,n-1);
+	printf ("Sorted Array: ");
+	for (i=0;i<n;i++)
+		printf ("%d\t", a[i]);
+	
+}
