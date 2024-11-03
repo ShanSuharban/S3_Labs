9e948dfe1c81b42231d9c0484d862b79105f5f0e
Muhammed Shan <95335433+ShanSuharban@users.noreply.github.com>
2024-11-03 17:25:30 +0530

    Update and rename TestRead.java to ReadChar.java

--- java/files/ReadChar.java
+++ java/files/ReadChar.java
@@ -1,11 +1,11 @@
 import java.io.*;
-public class TestRead
+public class ReadChar
 {
 	public static void main(String[] args)
 	{
 		try
 		{
-			FileInputStream f = new FileInputStream("/home/s3bcs17/oops/Test.txt");
+			FileInputStream f = new FileInputStream("Test.txt"); // provide path here 
 			System.out.println(f.read());
 			f.close();
 		}
