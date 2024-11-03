6ce8c7b3092b1b561eda7f91dd091d6586efe1bf
Muhammed Shan <95335433+ShanSuharban@users.noreply.github.com>
2024-11-03 17:35:43 +0530

    Update and rename TestCopyRead.java to ReadCopyChar.java

--- java/files/ReadCopyChar.java
+++ java/files/ReadCopyChar.java
@@ -1,11 +1,11 @@
 import java.io.*;
-public class TestCopyRead
+public class ReadCopyChar
 {
 	public static void main(String[] args) throws IOException
 	{
 		try
 		{
-			FileInputStream f = new FileInputStream("/home/s3bcs17/oops/TestDemo.txt");
+			FileInputStream f = new FileInputStream("TestDemo.txt");
 			System.out.println(f.read());
 			f.close();
 		}
