d96927170c23a17082464248253d2a8a5ef86f74
Muhammed Shan <95335433+ShanSuharban@users.noreply.github.com>
2024-11-11 18:49:52 +0530

    Update QuickSort.c

--- ds_c/QuickSort.c
+++ ds_c/QuickSort.c
@@ -18,21 +18,26 @@ int partition(int arr[], int low, int high) {
 
         // Find the first element greater than
         // the pivot (from starting)
-        while (arr[i] <= p && i <= high - 1) {
+        //getting largest element from the left sub array of pivot inorder to swap
+        while (arr[i] <= p && i <= high - 1) { 
+            
             i++;
         }
 
         // Find the first element smaller than
         // the pivot (from last)
-        while (arr[j] > p && j >= low + 1) {
+        // getting smallest element from the right sub array of pivot inorder to swap
+        while (arr[j] > p && j >= low + 1) { // here j>=low+1 is used inorder to avoid comparing with pivot
             j--;
         }
         if (i < j) {
-            swap(&arr[i], &arr[j]);
+            swap(&arr[i], &arr[j]); // swapping with i and j -> inorder to get "<=pivot pivot >pivot" order  
         }
-    }
-    swap(&arr[low], &arr[j]);
-    return j;
+    } // continuing until i and j crosses , then it is exited from the loop
+    // so the j position is actually the correct position of the pivot
+    //since we set pivot as arr[low] initially , it need to change with j position.
+    swap(&arr[low], &arr[j]); //actual pivot position get from here
+    return j; // so from here the array is partitioned into left sub array contains values <=pivot and right sub array contains values greater than pivot in mixed order.
 }
 
 void quickSort(int arr[], int low, int high) {
@@ -43,8 +48,9 @@ void quickSort(int arr[], int low, int high) {
 
         // Recursively call quickSort() for left and right
         // half based on Partition Index
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
+        //since we already got the pivot position , we dont need to sort it again , so "low to (pi-1)" in left sub array to get their pivot and "(pi+1) to high" for right sub array to get their pivots  
+        quickSort(arr, low, pi - 1); // seperately sorting the left sub array (<pivot elements) recursively
+        quickSort(arr, pi + 1, high); // seperately sorting the right sub array (>pivot elements) recursively
     }
 }
 
